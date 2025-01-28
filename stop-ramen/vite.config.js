f2957f9bf5ad78216b84b356e9673361d3e32487
Cat-JiYoung <kimyamae82@gmail.com>
2025-01-28 20:14:00 +0900

    [FIX] proxy base url target 변경

--- stop-ramen/vite.config.js
+++ stop-ramen/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `${import.meta.env.VITE_BASE_URL}`,
+        target: import.meta.env.VITE_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
